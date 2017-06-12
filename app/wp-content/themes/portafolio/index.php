f63d6c14eca7945a9800143aa30f4f702d088bd1
Clemens Hemmerling <clemens@galileo.edu>
2017-06-12 15:29:23 -0600

    Agregamos css

--- app/wp-content/themes/portafolio/index.php
+++ app/wp-content/themes/portafolio/index.php
@@ -5,10 +5,11 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo(title); ?></title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
     <link rel="stylesheet" href="css/foundation.css">
-    <link rel="stylesheet" href="<?php bloginfo(stylesheet_url);">
+    <link rel="stylesheet" href="<? php bloginfo(stylesheet_url); ?>">
   </head>
   <body>
     <h1><?php bloginfo(name); ?></h1>
