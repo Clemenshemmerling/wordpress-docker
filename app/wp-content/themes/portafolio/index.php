1c3fa546aa9fcee1c8857e80e282009b4b63116b
Clemens Hemmerling <clemens@galileo.edu>
2017-06-12 22:05:18 -0600

    Creacion de loop con filtro

--- app/wp-content/themes/portafolio/index.php
+++ app/wp-content/themes/portafolio/index.php
@@ -3,21 +3,53 @@
       <div class="column small-12 medium-9">
         <h3>Últimos trabajos</h3>
         <?php
-            if ( have_posts() ) {
-              while (have_posts()) {
-                the_post();
-                ?>
-                <article>
-                  <h2><?php the_title(); ?></h2>
-                  <p><?php the_excerpt(); ?></p>
-                  <footer>
-                    <small><?php the_tags(); ?> <?php the_author(); ?></small>
-                  </footer>
-                </article>
-                <?php
-              }
-            }
+          $args = array('cat' => 6, 'author' => "admin", 'posts_per_page' => 1);
+          $filter_post = new WP_Query($args);
+
+          if ($filter_post->have_posts() ):
+            while ($filter_post->have_posts() ):
+              $filter_post->the_post();
          ?>
+         <article>
+          <h2><?php the_title(); ?></h2>
+          <p><?php the_excerpt(); ?></p>
+          <footer>
+            <small><?php the_tags(); ?> <?php the_author(); ?></small>
+          </footer>
+         </article>
+         <?php
+           endwhile;
+           else :
+          ?>
+          <h4>No se encontraron entradas</h4>
+          <?php
+            endif;
+            wp_reset_postdata();
+           ?>
+           <?php
+             $args = array('cat' => 4, 'author' => "admin");
+             $filter_post = new WP_Query($args);
+
+             if ($filter_post->have_posts() ):
+               while ($filter_post->have_posts() ):
+                 $filter_post->the_post();
+            ?>
+            <article>
+             <h2><?php the_title(); ?></h2>
+             <p><?php the_excerpt(); ?></p>
+             <footer>
+               <small><?php the_tags(); ?> <?php the_author(); ?></small>
+             </footer>
+            </article>
+            <?php
+              endwhile;
+              else :
+             ?>
+             <h4>No se encontraron entradas</h4>
+             <?php
+               endif;
+               wp_reset_postdata();
+              ?>
       </div>
       <?php get_sidebar(); ?>
     </div>
